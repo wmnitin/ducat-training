aa3d7da07e671f70bd281bf79983c54da1c9c91b
wmnitin <wmnitin@gmail.com>
2020-01-22 20:31:05 +0530

    'update'

--- src/components/21jan/Screen.js
+++ src/components/21jan/Screen.js
@@ -1,14 +1,44 @@
 import React from 'react';
 
 export default class Screen extends React.PureComponent {
-    data='2019'
-    componentDidMount(){
+    data = '2019'
+    state = {
+        myVar: [{
+            name: 'nitin'
+        }, {
+            name: 'sujal'
+        }, {
+            name: 'sudfsdjal'
+        }],
+        myInputValue: ''
+    }
+    deletesome=()=>{
+        let newArr = [...this.state.myVar]
+        newArr.splice(1,1)
+        this.setState({
+            myVar: newArr
+        })
+    }
+
+    changeMe=(event)=>{
+        this.setState({
+            myInputValue: event.target.value
+        })
+    }
+    componentDidMount() {
         this.props.getDataFromPhone(this.data)
     }
     render() {
         return (
             <div>
-                <h1>Screen name is {this.props.screenName}</h1>
+                Enter here: <input value={this.state.myInputValue} onChange={this.changeMe} />
+                <br/>
+                {this.state.myInputValue}
+                <br/>
+                <h1 onClick={this.deletesome}>Screen name is {this.props.screenName}</h1>
+                {
+                   JSON.stringify(this.state.myVar)
+                }
             </div>
         )
     }
--- src/components/21jan/User.js
+++ src/components/21jan/User.js
@@ -3,17 +3,28 @@ import Phone from './Phone'
 
 export default class User extends React.PureComponent {
 
+    buttonRef = React.createRef();
+
     state = {
         phoneName: 'Smasung s8',
         screenName: 'Gorilla'
     }
 
+    callMe = (name, event) =>{
+        console.log('helloooooooooo')
+
+        this.buttonRef.current.innerHTML = 'Someone has clicked :D'
+
+    }
+
     render() {
         return (
             <div>
-                <h1>I am user</h1>
+                <h1 ref={this.buttonRef}>No one has clicked :(</h1>
                 <Phone name={this.state.phoneName}
                     screenName={this.state.screenName} />
+
+                    <button onClick={this.callMe.bind(this, 'nitin')}>Click Me</button>
             </div>
         )
     }
